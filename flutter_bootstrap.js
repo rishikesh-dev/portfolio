c9bd4acf99c7b32f5f7d70892155d87f2e0c3a1c
RISHIKESH-RICHI <rishikeshdeveloperrichi@gmail.com>
2025-06-05 15:33:55 +0700

    fix

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2719345762"
+    serviceWorkerVersion: "3890083386"
   }
 });
